441163f7a09c00d0cece7733ca64cb70d7efd153
OANH TRAN <45414817+oanh098@users.noreply.github.com>
2020-02-18 00:10:18 +0700

    2020Feb17 add Global Site Tag as the first item into the <HEAD>

--- resources/views/incPages/head.blade.php
+++ resources/views/incPages/head.blade.php
@@ -1,4 +1,13 @@
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158597454-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-158597454-1');
+    </script>
     <meta charset="utf-8">
     <!-- Scripts -->
 {{--    <script src="{{ asset('js/app.js') }}" defer></script>--}}
