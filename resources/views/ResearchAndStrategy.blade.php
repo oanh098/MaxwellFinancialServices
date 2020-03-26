3b7c68372c1a53ead4a8391a9bd9b730e0496516
OANH TRAN <45414817+oanh098@users.noreply.github.com>
2020-03-26 09:36:56 +0700

    2020March26 check spelling, welcome mail, crossing banner

--- resources/views/ResearchAndStrategy.blade.php
+++ resources/views/ResearchAndStrategy.blade.php
@@ -8,7 +8,7 @@
                 <div class="html_content">
                     <p style="margin:0.7em 0em 0em 0em;">
                         <span style="font-family:'Open Sans','Arial'; font-weight:300;">
-                            Maxwell Financial Services overs Clients an extensive range of international investing. People often invest internationally to broaden diversification greater returns Tax incentives and less risk among foreign markets and companies. MFS has their own dedicated team working within emerging markets, over the last few years we have shown exceptional results for our investors within the artificial intelligence and medical marijuana sectors within the international emerging markets. Please speak to your Specialist at Maxwell Financial Services
+                            Maxwell Financial Services offers Clients an extensive range of international investing. People often invest internationally to broaden diversification greater returns Tax incentives and less risk among foreign markets and companies. MFS has their own dedicated team working within emerging markets, over the last few years we have shown exceptional results for our investors within the artificial intelligence and medical marijuana sectors within the international emerging markets. Please speak to your Specialist at Maxwell Financial Services
                         </span>
                     </p>
 
