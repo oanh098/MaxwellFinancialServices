cb7cedf415b683b772cc7d450fe63f92df8e4e2b
OANH TRAN <45414817+oanh098@users.noreply.github.com>
2020-04-01 22:24:50 +0700

    2020April1, json data

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -39,6 +39,7 @@
     <title>
         {{$headerTitle ?? 'Home - Maxwell Financial Services'}}
     </title>
+    <meta name="google-site-verification" content="google-site-verification=k0Q7hbIkkspS1tjNEoOB6GcbnndtU0t2_ktvJx0DulQ">
     <link rel="stylesheet"  href="{{asset('FrontEnd')}}/bootstrap.css" type="text/css" media="screen"/>
     <link rel="stylesheet"  href="{{asset('FrontEnd')}}/stylebck.css" type="text/css" media="screen"/>
     <link rel="stylesheet"  href="{{asset('FrontEnd')}}/extraStyle.css" type="text/css" media="screen"/>
