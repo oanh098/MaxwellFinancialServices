6bc4d24873beb2f287cc5929e948c5eba63bb39c
OANH TRAN <45414817+oanh098@users.noreply.github.com>
2020-06-03 12:59:16 +0700

    2020June3, add again logo 3aw logo

--- resources/views/inc/ourCustomer.blade.php
+++ resources/views/inc/ourCustomer.blade.php
@@ -2,10 +2,7 @@
 <div class="row" style="padding-top: 3em; padding-bottom: 3em" >
     <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="ttr_index_html_column60">
-            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
-            <div class="html_content"><p style="text-align:Center;"><span style="font-size:2em;color:rgba(31,31,31,1);">Our Affiliated Clientele</span></p></div>
-            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
-            <div style="clear:both;"></div>
+                <p style="text-align:Center;font-size:2em">Our Affiliated Clientele</p>
         </div>
     </div>
     <div class=" visible-lg-block visible-sm-block visible-md-block visible-xs-block" style="clear: both;"></div>
@@ -14,47 +11,48 @@
 
 <div class="row" >
     <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
-            <div class="html_content"><span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 0em 0em;"><span>
-                        <img class="ttr_uniform" style="max-height:128px;" src="{{asset('FrontEnd')}}/images/westpac-logo.png" alt="wespac" /></span></span></div>
+                        <img class="img-fluid" src="{{asset('FrontEnd')}}/images/westpac-logo.png" alt="wespac" />
     </div>
 
     <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
-            <div class="html_content"><span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 0em 0em;"><span>
-                            <img class="ttr_uniform" style="max-height:128px;" src="{{asset('FrontEnd')}}/images/citibank.png" /></span></span></div>
+            <div class="img-fluid">
+                            <img class="img-fluid" src="{{asset('FrontEnd')}}/images/citibank.png" /></div>
     </div>
 
     <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
-            <div class="html_content"><span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 0em 0em;"><span>
-                        <img class="ttr_uniform" style="max-height:128px;" src="{{asset('FrontEnd')}}/images/commonwealth-bank.png" alt="commonwealthbank" /></span></span></div>
+            <div class="img-fluid">
+                        <img class="img-fluid" style="max-height:128px;" src="{{asset('FrontEnd')}}/images/commonwealth-bank.png" alt="commonwealthbank" /></div>
     </div>
 
     <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
-            <div class="html_content">
-                <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 0em 0em;"><span>
-                        <img class="ttr_uniform" style="max-height:128px;max-width:128px;" src="{{asset('FrontEnd')}}/images/nab.png" alt="nab" /></span></span></div>
+            <div class="img-fluid">
+                        <img class="img-fluid"  src="{{asset('FrontEnd')}}/images/nab.png" alt="nab" /></div>
     </div>
 
     <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
-            <div class="html_content">
-                <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 0em 0em;"><span>
-                            <img class="ttr_uniform" style="max-height:128px;max-width:128px;" src="{{asset('FrontEnd')}}/images/hsbc.png" alt="hsbc" /></span></span></div>
+            <div class="img-fluid">
+                            <img class="img-fluid"  src="{{asset('FrontEnd')}}/images/hsbc.png" alt="hsbc" /></div>
     </div>
     <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
-            <div class="html_content">
-                <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 0em 0em;"><span><img class="ttr_uniform" style="max-height:128px;max-width:128px;" src="{{asset('FrontEnd')}}/images/anz.png" alt="anz" /></span></span></div>
+            <div class="img-fluid">
+                        <img class="img-fluid"  src="{{asset('FrontEnd')}}/images/anz.png" alt="anz" /></div>
     </div>
 
     <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
-            <div class="html_content">
-                <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 0em 0em;"><span><img class="ttr_uniform" style="max-height:128px;max-width:128px;" src="{{asset('FrontEnd')}}/images/61171.png" /></span></span></div>
+            <div class="img-fluid">
+                        <img class="img-fluid"  src="{{asset('FrontEnd')}}/images/61171.png" /></div>
     </div>
     <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
-            <div class="html_content">
-                <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 0em 0em;"><span><img class="ttr_uniform" style="max-height:128px;max-width:128px;" src="{{asset('FrontEnd')}}/images/eBay-Logo2.png" alt="ebay" /></span></span></div>
+            <div class="img-fluid">
+                        <img class="img-fluid" src="{{asset('FrontEnd')}}/images/eBay-Logo2.png" alt="ebay" /></div>
     </div>
     <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
-            <div class="html_content">
-                <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 0em 0em;"><span><img class="ttr_uniform" style="max-height:128px;max-width:128px;" src="{{asset('FrontEnd')}}/images/xe.png" alt="xe" /></span></span>.</div>
+            <div class="img-fluid">
+                        <img class="img-fluid"  src="{{asset('FrontEnd')}}/images/3aw.png" alt="ebay" /></div>
+    </div>
+    <div class=" col-lg-2 col-md-2 col-sm-3 col-xs-3">
+            <div class="img-fluid">
+                        <img class="img-fluid"  src="{{asset('FrontEnd')}}/images/xe.png" alt="xe" /></div>
         </div>
 
     </div>
